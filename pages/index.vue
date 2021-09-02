960e097d9a9ad81d6c011b31732d64af8757e1cf
AugustodeAraujo <poxaugustodantas@gmail.com>
2021-09-02 17:41:50 -0300

    Initial Commit Module 02

---
+++ pages/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div
+      v-for="home in homes"
+      :key="home.objectID"
+      style="float:left; margin: 10px"
+    >
+      <nuxt-link :to="`/home/${home.objectID}`">
+        <HomeCard :home="home"></HomeCard
+      ></nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import homes from '~/data/homes'
+
+export default {
+  head() {
+    return {
+      title: 'Homepage',
+      meta: [
+        {
+          name: 'description',
+          content: 'This is a homepage!',
+          hid: 'description',
+        },
+      ],
+    }
+  },
+  data() {
+    return {
+      homes: homes,
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
